0613e3ca625e749bce2fe92b86c4ba399cc39ea6
Iago Queiroz <iago.queiroz@hotmail.com>
2020-09-04 06:35:09 +0000

    Setting up dependency container

---
+++ api/app/settings.php
@@ -0,0 +1,12 @@
+<?php
+declare (strict_types = 1);
+
+use DI\ContainerBuilder;
+
+return function (ContainerBuilder $containerBuilder) {
+    $containerBuilder->addDefinitions([
+        'settings' => [
+            'displayErrorDetails' => true,
+        ],
+    ]);
+};
--- api/public/index.php
+++ api/public/index.php
@@ -1,12 +1,27 @@
 <?php
+use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$containerBuilder = new ContainerBuilder;
+
+// Set up settings
+$settings = require __DIR__ . '/../app/settings.php';
+$settings($containerBuilder);
+
+// Build PHP-DI container instance
+$container = $containerBuilder->build();
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
+// Add Routing Middleware
 $app->addRoutingMiddleware();
-$app->addErrorMiddleware(true, true, false);
+
+// Add Error Handling Middleware
+$displayErrorDetails = $container->get('settings')['displayErrorDetails'];
+$app->addErrorMiddleware($displayErrorDetails, false, false);
 
 // Register routes
 $routes = require __DIR__ . '/../app/routes.php';
