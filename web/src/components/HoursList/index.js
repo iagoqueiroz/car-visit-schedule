aef1dbf33b1241f0a9e992751503453da91e3912
Iago Queiroz <iago.queiroz@hotmail.com>
2020-09-08 17:50:24 -0300

    Disabling already scheduled times

--- web/src/components/HoursList/index.js
+++ web/src/components/HoursList/index.js
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
 
-const HoursList = ({ onSelectHour }) => {
-  const [hours, setHours] = useState([
-    { value: "09:00", selected: false },
-    { value: "10:00", selected: false },
-    { value: "11:00", selected: false },
-    { value: "14:00", selected: false },
-    { value: "15:00", selected: false },
-    { value: "16:00", selected: false },
-    { value: "17:00", selected: false },
-  ]);
+const HoursList = ({ onSelectHour, disableds = [] }) => {
+  const [hours, setHours] = useState([]);
+
+  useEffect(() => {
+    const hourArr = [
+      { value: "09:00", selected: false, disabled: false },
+      { value: "10:00", selected: false, disabled: false },
+      { value: "11:00", selected: false, disabled: false },
+      { value: "14:00", selected: false, disabled: false },
+      { value: "15:00", selected: false, disabled: false },
+      { value: "16:00", selected: false, disabled: false },
+      { value: "17:00", selected: false, disabled: false },
+    ];
+
+    setHours([
+      ...hourArr.map((hour) => ({
+        ...hour,
+        disabled: disableds.includes(hour.value),
+      })),
+    ]);
+  }, [disableds]);
 
   function handleClick(hour) {
+    if (hour.disabled) return;
+
     setHours(
       hours.map((h) => {
         return { ...h, selected: h === hour ? true : false };
@@ -29,7 +42,9 @@ const HoursList = ({ onSelectHour }) => {
         <div
           onClick={() => handleClick(hour)}
           key={index}
-          className={`hour-item${hour.selected ? " active" : ""}`}
+          className={`hour-item${hour.selected ? " active" : ""}${
+            hour.disabled ? " disabled" : ""
+          }`}
         >
           {hour.value}
         </div>
--- web/src/components/Steps/FirstStep/index.js
+++ web/src/components/Steps/FirstStep/index.js
@@ -1,8 +1,9 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 import moment from "../../../utils/moment";
 
 import { FormContext } from "../../../contexts/FormContext";
+import { CarContext } from "../../../contexts/CarContext";
 
 import DaysList from "../../DaysList";
 import HoursList from "../../HoursList";
@@ -11,8 +12,26 @@ import Button from "../../Button";
 const FirstStep = (props) => {
   const [date, setDate] = useState("");
   const [hour, setHour] = useState("");
+  const [disableds, setDisableds] = useState([]);
 
   const { form, setForm, setStep } = useContext(FormContext);
+  const { schedules } = useContext(CarContext);
+
+  useEffect(() => {
+    if (schedules.length && date) {
+      const daySchedules = schedules.filter((schedule) =>
+        moment(date)
+          .startOf("day")
+          .isSame(moment(schedule.visit_date).startOf("day"))
+      );
+
+      setDisableds(
+        daySchedules.map((schedule) =>
+          moment(schedule.visit_date).format("HH:mm")
+        )
+      );
+    }
+  }, [schedules, date]);
 
   function nextStep(e) {
     e.preventDefault();
@@ -31,7 +50,10 @@ const FirstStep = (props) => {
       <div className="card-body">
         <div className="card-subtitle">{moment().format("MMMM [de] YYYY")}</div>
         <DaysList onSelectDate={(date) => setDate(date)} />
-        <HoursList onSelectHour={(hour) => setHour(hour)} />
+        <HoursList
+          onSelectHour={(hour) => setHour(hour)}
+          disableds={disableds}
+        />
         <div className="card-button">
           <Button onClick={nextStep} disabled={!date || !hour}>
             Agendar Visita
