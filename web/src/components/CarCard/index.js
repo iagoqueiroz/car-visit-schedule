8a04e9c959032bc9790cf6f0ecdc13492c970d6f
Iago Queiroz <iago.queiroz@hotmail.com>
2020-09-08 03:12:05 -0300

    API service and fetching car information

--- web/src/components/CarCard/index.js
+++ web/src/components/CarCard/index.js
@@ -1,28 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MdLocationOn } from "react-icons/md";
+
+import api from "../../services/api";
 
 import "./styles.css";
 
-const CarCard = (props) => (
-  <div className="card car-card">
-    <div className="card-image">
-      <img
-        src="https://cdn-motorshow-ssl.akamaized.net/wp-content/uploads/sites/2/2020/04/26.jpg"
-        alt="Fiat Argo"
-      />
-    </div>
-    <div className="card-title">Fiat Argo</div>
-    <div className="card-body">
-      FLEX 1.0 8V MANUAL 4P
-      <div className="price">R$ 13.000,00</div>
-    </div>
-    <div className="card-footer">
-      <div className="location">
-        <MdLocationOn />
-        Fortaleza - CE
+const CarCard = (props) => {
+  const [car, setCar] = useState({});
+
+  useEffect(() => {
+    const fetchCar = async () => {
+      try {
+        const { data } = await api.get("/cars/1");
+
+        setCar(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchCar();
+  }, []);
+
+  return (
+    <div className="card car-card">
+      <div className="card-image">
+        <img
+          src="https://cdn-motorshow-ssl.akamaized.net/wp-content/uploads/sites/2/2020/04/26.jpg"
+          alt={`${car.company} ${car.model}`}
+        />
+      </div>
+      <div className="card-title">
+        {car.company} {car.model}
+      </div>
+      <div className="card-body">
+        {car.description}
+        <div className="price">
+          {Number(car.price).toLocaleString("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          })}
+        </div>
+      </div>
+      <div className="card-footer">
+        <div className="location">
+          <MdLocationOn />
+          Fortaleza - CE
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CarCard;
